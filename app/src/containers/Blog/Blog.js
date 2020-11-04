1972af1e886efe918c2c6179544f44b433660143
stevekutz <stkutz@gmail.com>
2020-11-03 18:48:51 -0600

    sliced portion of response data, added property, and rendered to Post comp

--- app/src/containers/Blog/Blog.js
+++ app/src/containers/Blog/Blog.js
@@ -5,14 +5,64 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './blog.module.css';
 
+import axios from 'axios';
+
 class Blog extends Component {
+    state = {
+        posts: [],            
+                
+    
+    }
+
+
+
+
+    // using Lifecycle hooks to fetch data from 'server'
+    // axios using Promises due to async nature of requests
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response)
+                // MUST updated state inside .then
+
+                // only take first 4 data items and convert author to 'Joe'
+                const posts_copy = response.data.slice(0,4)
+                const modifiedPosts = posts_copy.map(post => {
+                    return {
+                        ...post,
+                        author: 'Joe'
+                    }
+                
+                })
+
+
+
+                this.setState({posts: modifiedPosts})
+            })
+
+    }
+
+
+
+
     render () {
+
+    // map posts data from state into Post component
+        const posts = this.state.posts.map(post => {
+            return <Post 
+                    key = {post.id}
+                    title = {post.title}
+                    author = {post.author}
+                    
+                    
+                    />     
+        
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
